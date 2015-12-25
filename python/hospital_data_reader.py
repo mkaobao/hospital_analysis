dd100c7106738e7097a5895f9e4465a53d4b286c
XDTZ <b97401009@ntu.edu.tw>
2015-12-25 22:59:20 +0800

    Working on improved time-experience-weighted estimation
    Unfinished. DO NOT PULL.

--- python/hospital_data_reader.py
+++ python/hospital_data_reader.py
@@ -131,22 +131,32 @@ def listDoctorForExport(params):
 
 def listDoctorForOrderExport(params):
     #Exporting format:
-    #number, date, time, interval, order(actual order meeting doctor), isPassed(whether the patient is late, 0 == False, 1 == True), passedCount(including missing
-    # ones), lateCount, lateOriginalOrder: 遲到者原本的Order, Duration
+    #number, date, time, interval, order(actual order meeting doctor), isPassed(whether the patient is late, 0 == False, 1 == True), passedCount(including missing ones), lateCount, lateOriginalOrder: 遲到者原本的Order, Duration, diffEst, Est
 
     output = None
     result = DB.searchByParams(params)
     curInterval = u''
     curDate = ''
+    lastRegularStart = None #last start time datetime object
     lastRegularNumber = 0
     order = 1
     passedCount = 0
     curDate = ''
     curInterval = ''
     signBook = [] #record the number that did came to the doctor
-    passedSignBook = {} #record the passed patients' original order
+    lateSignBook = {} #record the passed patients' original order
     sessionData = []
-    
+    summary = []
+    peopleSeconds = 0 #record the people*seconds of passed patients, as the denominator for the frequeny of late person showing up
+    totalPassedCount = 0
+    durationSum = 0 #As the denominator for calculating average session time
+    #以每小時紀錄略過號碼的比例
+    #Format: 診次:{小時:{pass:A, total:X}}
+    passProbHour = {'上午診':{}, '下午診':{}, '夜間診':{}}
+    #以每小時紀錄遲到者出現的比例
+    #Format: 診次:{小時:{appeared:A, denominator:X}}
+    recoverProbHour = {'上午診':{}, '下午診':{}, '夜間診':{}}
+
     #Signal as EOF
     result.append(['','1970-1-1','','','','','',0,0,0,0])
 
@@ -165,12 +175,12 @@ def listDoctorForOrderExport(params):
         number   = int(row[7])
         start       = datetime.datetime.fromtimestamp(int(row[8])).strftime('%H:%M:%S')
         duration    = transfer_minute(row[10])
+        durationSeconds = row[10]
 
         if output == None:
             line = u'%s/%s-%s.txt' % (params['filePath'].encode('utf-8').decode('utf-8'), dept.encode('utf-8').decode('utf-8'), name.encode('utf-8').decode('utf-8'))
             output = open(line, 'w')
-            output.write('number\tdate\ttime\tinterval\torder\tisPassed\tpassedCount\tpassedOriginalOrder\tlateCount'
-                         '\tduration\n')
+            output.write('number\tdate\ttime\tinterval\torder\tisPassed\tpassedCount\tlateCount\tlateOriginalOrder\tduration\n')
 
         #Initialization for each intervals, and the outputting of data when each interval is processed
         #1. Read everything into sessionData.
@@ -195,48 +205,116 @@ def listDoctorForOrderExport(params):
                     else:
                         sessionData[i]['lateCount'] = lastLateCount - 1
                 for p in sessionData:
-                    output.write('%d\t%s\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%s\n' % (p['number'] , p['date'] , p['start'] ,p['interval'] , p['order'] , p['isPassed'] ,
-                                                                           p['passedCount'], p['lateCount'], p['lateOriginalOrder'], p['duration']))
+                    output.write('%d\t%s\t%s\t%s\t%d\t%d\t%d\t%d\t%d\t%s\n' % (p['number'] , p['date'] , p['start'] ,p['interval'] , p['order'] , p['isPassed'] , p['passedCount'], p['lateCount'], p['lateOriginalOrder'], p['duration']))
 
-            #Formatting the dictionary for each rows
+            if(len(sessionData) > 1):
+                summary.append([sessionData[len(sessionData)-1]['number'],sessionData[len(sessionData)-1]['passedCount']])
+
+            #Formatting the dictionary for each
             sessionData = []
             signBook = []
-            passedSignBook = {}
+            lateSignBook = {}
             curInterval = interval
             curDate = date
             lastRegularNumber = 0
             order = 1
             passedCount = 0
-           
+            lastRegularStart = None
+
         if number in signBook:########### WHAT TO DO WITH THESE GUYS WHO JUST WON'T LEAVE!!??? (Some of them with good reasons maybe)###############
             continue
-        
+
         #PASSED : decrease passedCount
         #if row[6] == '{"over":true}': # <== This is problematic. Some times patients were skipped too fast that regular patient would be viewed as passed by SYSTEM
         elif number < lastRegularNumber:
             passedCount -= 1
-            try:
-                sessionData.append({'number':number,'date':date.encode('utf-8'),'start':start.encode('utf-8'),
-                                'interval':interval.encode('utf-8'),'order':order,'isPassed':1,
-                                'passedCount':passedCount,'lateCount':0, 'lateOriginalOrder':passedSignBook[number], 'duration':duration})
-            except KeyError:
-                print 'KeyError:', number, '\n', name, date, interval, '\n', passedSignBook
+            sessionData.append({'number':number,'date':date.encode('utf-8'),'start':start.encode('utf-8'),'interval':interval.encode('utf-8'),'order':order,'isPassed':1,'passedCount':passedCount,'lateCount':0, 'lateOriginalOrder':lateSignBook[number], 'duration':duration})
 
         #Regular: might encounter passing
-        else: 
-            if lastRegularNumber != number - 1:
-                delta = number - lastRegularNumber
+        else:
+            delta = number - lastRegularNumber
+            if delta != 1:
                 passedCount += delta - 1
+                if start.hour in passProbHour[interval]:
+                    passProbHour[interval][start.hour]['pass'] += delta -1
+                    passProbHour[interval][start.hour]['total'] += delta
+                else:
+                    passProbHour[interval][start.hour] = {'pass':delta - 1, 'total': delta}
+                if start.hour in recoverProbHour[interval] and passedCount > 0:
+                    recoverProbHour[interval][start.hour]['denominator'] += passedCount * (start - lastRegularStart).total_seconds()
+
                 for steps in range(1, delta):
-                    passedSignBook[number - steps] = order
+                    lateSignBook[number - steps] = order
+            else:
+                if start.hour in passProbHour[interval]:
+                    passProbHour[interval][start.hour]['total'] += 1
+                else:
+                    passProbHour[interval][start.hour] = {'pass': , 'total': 1}
+
             lastRegularNumber = number
-            sessionData.append({'number':number,'date':date.encode('utf-8'),'start':start.encode('utf-8'),'interval':interval.encode('utf-8'),'order':order,
-                                'isPassed':0,'passedCount':passedCount,'lateCount':0, 'lateOriginalOrder':0, 'duration':duration})
+            lastRegularStart = start
+            sessionData.append({'number':number,'date':date.encode('utf-8'),'start':start.encode('utf-8'),'interval':interval.encode('utf-8'),'order':order,'isPassed':0,'passedCount':passedCount,'lateCount':0, 'lateOriginalOrder':0, 'duration':duration})
+
         signBook.append(number)
         order += 1
+        peopleSeconds += sessionData[len(sessionData)-1]['passedCount'] * durationSeconds
+        totalPassedCount += sessionData[len(sessionData)-1]['isPassed']
+        durationSum += durationSeconds
 
     if output != None:
         output.close()
+
+    #Estimating session time for patients with order > 10
+    #Target: Whenever meeting a guy with order > skip+vision, and who was not a late patient, we trace back [vision] patients and try using the information back then to estimate his session time. This way we make sure we got the actual answer for on time patients, as we expected of the end-user.
+    skipping = 0
+    total = 0
+    for p in summary:
+        #output.write('%d\t%d\n' % (p[0], p[1]))
+        skipping += p[1]
+        total += p[0]
+    averageSkip = float(skipping) / total #其實這是計算完全消失的比例而非跳號的可能性，時間會高估。但是也可以說假設這些短期遲到的馬上都跑回來看了
+    averageTime = durationSum / total
+    reappearanceFrequency = float(totalPassedCount) / peopleSeconds
+
+    with open(line,'rw+') as fp:
+        content = fp.readlines()
+        fp.seek(0)
+        fp.truncate()
+        #Write back the header from the input stream and adding another column for difference between estimation and acutal session time
+        fp.write(content[0].rstrip() + '\tdiffForEst\test\n')
+
+        #lastOrder = 0
+        #lastDate = ''
+        #lastInterval = ''
+        lastRegularNumber = 0
+        skip = 10 #How many patients skipped before start estimating
+        vision = 10 #How far we are trying to estimate
+        for i in range(1, len(content)):# Starting from 1 skipping the header
+        #Column format: 0:number\1:date\2:time\3:interval\4:order\5:isPassed\6:passedCount\7:lateCount\8:lateOriginalOrder\9:duration\10:answer-etimation\11:estimation
+            items = content[i].rstrip().split('\t')
+            #if items[1] != lastDate or items[3] != lastInterval
+                #some preparations of next interval
+            if int(items[4]) < skip + vision or int(items[5]) == 1:
+                fp.write(content[i].rstrip() + '\tNA\tNA\n')
+                continue
+            else:
+                j = i-vision
+                pastItems = content[j].rstrip().split('\t')
+                lastRegularNumber = int(pastItems[0])
+                isPassed = int(pastItems[5])
+                while isPassed == 1:#Finding the closest regular patient near the start of vision
+                    j -= 1
+                    backTraceItems = content[j].rstrip().split('\t')
+                    isPassed = int(backTraceItems[5])
+                    lastRegularNumber = int(backTraceItems[0])
+                #預期準時扣掉會遲到的人
+                estimatedRegular = math.floor((int(items[0]) - lastRegularNumber) * (1.0 - averageSkip))
+                #預期已經遲到但是又會再出現的人
+                estimatedReappearance = math.floor(int(pastItems[6]) * averageTime * estimatedRegular * reappearanceFrequency)
+                estimation = (estimatedRegular + estimatedReappearance) * averageTime
+                answer = (datetime.datetime.strptime(items[2],'%H:%M:%S') - datetime.datetime.strptime(pastItems[2],'%H:%M:%S')).total_seconds()
+                diff = answer - estimation
+                fp.write(content[i].rstrip() + '\t%d\t%d\n' % (diff,estimation))
 
 def export(filePath):
     doctorList = DB.getDoctorList()
